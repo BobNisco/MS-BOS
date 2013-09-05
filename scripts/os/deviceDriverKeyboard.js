e4df7d09df1d0512ebd9ab45f195de6a27b59f47
Bob Nisco <BobNisco@gmail.com>
2013-09-05 19:10:08 -0400

    Handle some punctuation

--- scripts/os/deviceDriverKeyboard.js
+++ scripts/os/deviceDriverKeyboard.js
@@ -1,8 +1,8 @@
 /* ----------------------------------
    DeviceDriverKeyboard.js
-   
+
    Requires deviceDriver.js
-   
+
    The Kernel Keyboard Device Driver.
    ---------------------------------- */
 
@@ -36,7 +36,7 @@ function krnKbdDispatchKeyPress(params)
     if ( ((keyCode >= 65) && (keyCode <= 90)) ||   // A..Z
          ((keyCode >= 97) && (keyCode <= 123)) )   // a..z
     {
-        // Determine the character we want to display.  
+        // Determine the character we want to display.
         // Assume it's lowercase...
         chr = String.fromCharCode(keyCode + 32);
         // ... then check the shift key and re-adjust if necessary.
@@ -45,13 +45,59 @@ function krnKbdDispatchKeyPress(params)
             chr = String.fromCharCode(keyCode);
         }
         // TODO: Check for caps-lock and handle as shifted if so.
-        _KernelInputQueue.enqueue(chr);        
-    }    
-    else if ( ((keyCode >= 48) && (keyCode <= 57)) ||   // digits 
+        _KernelInputQueue.enqueue(chr);
+    }
+    else if ( ((keyCode >= 48) && (keyCode <= 57)) ||   // digits
                (keyCode == 32)                     ||   // space
                (keyCode == 13) )                        // enter
     {
         chr = String.fromCharCode(keyCode);
-        _KernelInputQueue.enqueue(chr); 
+        _KernelInputQueue.enqueue(chr);
+    } else if (isPunctuationChar(keyCode)) {
+        _KernelInputQueue.enqueue(handlePunctuationChar(keyCode, isShifted));
     }
+}
+
+function isPunctuationChar(ch) {
+    if ((ch >= 186 && ch <= 192) ||
+        (ch >= 219 && ch <= 222)) {
+        return true;
+    }
+    return false;
+}
+
+function handlePunctuationChar(keyCode, isShifted) {
+    // What is up with charcodes?
+    // Is there a function already made for this atrocity?
+    // If there is, I can't find it.
+    var lookupTable = {
+        '186' : ';',
+        '187' : '=',
+        '188' : ',',
+        '189' : '-',
+        '190' : '.',
+        '191' : '/',
+        '192' : '`',
+        '219' : '[',
+        '220' : '\\',
+        '221' : ']',
+        '222' : '\''
+    }, lookupTableShifted = {
+        '186' : ':',
+        '187' : '+',
+        '188' : '<',
+        '189' : '_',
+        '190' : '>',
+        '191' : '?',
+        '192' : '~',
+        '219' : '{',
+        '220' : '|',
+        '221' : '}',
+        '222' : '\"'
+    };
+    chr = lookupTable[keyCode];
+    if (isShifted) {
+        chr = lookupTableShifted[keyCode];
+    }
+    return chr;
 }
