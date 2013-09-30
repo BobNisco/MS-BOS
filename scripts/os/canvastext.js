086bbef46a46474948af6f4699ed3bea46ac14e7
Bob Nisco <BobNisco@gmail.com>
2013-09-30 01:41:00 -0400

    Console text color change after merging the other style changes

--- scripts/os/canvastext.js
+++ scripts/os/canvastext.js
@@ -153,7 +153,7 @@ CanvasTextFunctions.draw = function(ctx,font,size,x,y,str)
     ctx.save();
     ctx.lineCap = "round";
     ctx.lineWidth = 2.0 * mag;
-	ctx.strokeStyle = "#c5c8c6";
+	ctx.strokeStyle = "black";
 
     for (var i = 0; i < len; i++)
 	{
